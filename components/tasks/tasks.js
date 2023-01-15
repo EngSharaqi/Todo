cf9c37f8863c34ace030ebb5d15aab7dffd788e8
Mohamed A. Sharaqi <muhammadsharaqi@gmail.com>
2023-01-15 15:28:30 +0200

    css enhancements

--- components/tasks/tasks.js
+++ components/tasks/tasks.js
@@ -1,35 +1,39 @@
-import { Button, Col, Form, Input, Row } from 'reactstrap'
+import { Col, Spinner, Row } from 'reactstrap'
 import Card from './card'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import styles from '../../styles/Home.module.css'
-export default function Tasks () {
+export default function Tasks() {
     const [data, setData] = useState([])
-    
+
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/todos')
-        .then(todo => {
-            setData(todo.data)
-        })
-        .catch()
+            .then(todo => {
+                setData(todo.data)
+            })
+            .catch()
     }, [])
-    
+
     return (
         <Row className={styles.taskWrap}>
-            {/* <Col xs={12} md={5} lg={4}>
-                <Form onSubmit={(e) => e.preventDefault()}>
-                <Input onChange={(e) => console.log(e.target.value)} type='textarea' placeholder='What is the next todo ...?' required/>
-                    <select className='form-select'>
-                        <option value={'completed'}>Completed</option>
-                        <option value={'inCompleted'}>Not Completed</option>
-                    </select>
-                    <Button><i class="fas fa-plus"></i></Button>
-                </Form>
-            </Col> */}
             <Col xs={12} md={12} lg={12}>
-                <Card data = {data} />
+                {
+                    data == '' ?
+                        <div className={styles.spinner}>
+                            <Spinner
+                            color="primary"
+                            style={{
+                                height: '6rem',
+                                width: '6rem'
+                            }}
+                        >Loading...
+                        </Spinner>
+                        </div>
+                             :
+                        <Card data={data} />
+                }
             </Col>
         </Row>
     )
-    
+
 } 
